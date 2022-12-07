64d33f418073ffc89f130d6ebaf5dd01c2633edf
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-07 01:52:58 +0100

    delete all, delete single, change nr random email

--- js/main.js
+++ js/main.js
@@ -4,25 +4,33 @@ const { createApp } = Vue
         data() {
             return{
               emails : [],
-              email : ""
+              email : "",
+              nrEmail : 10
             }
         },
         methods:{
-          randomEmail(){
-            for ( let i = 0 ; i < 10 ; i++){
+          randomEmail(nrEmail){
+            this.emails = [] 
+            for ( let i = 0 ; i < nrEmail ; i++){
               axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
               .then((result)=>{
-                this.mail = result.data.response
-              console.log(this.mail);
+              this.mail = result.data.response
                this.emails.push(result.data.response)
               })
             }
         },
+        deleteEmail(indice){
+          if (confirm("Sei sicuro di voler cancellare questa email?") == true) {
+            this.emails.splice(indice,1)
+          } else {}
+        },
+        deleteAll(){
+          this.emails = [];
+        }
    
         },
         mounted(){
-          this.randomEmail();
-
+          this.randomEmail(this.nrEmail);
         }
 
     }).mount("#webapp")
